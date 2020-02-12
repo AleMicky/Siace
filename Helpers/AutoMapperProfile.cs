1b0ca3d25366c26e5aae45f88183908b001e2a0a
checIt-lab <57136184+checIt-lab@users.noreply.github.com>
2020-02-11 22:00:18 -0400

    first commit

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Siace.Entities;
+using Siace.Models.Users;
+
+namespace Siace.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserModel>();
+            CreateMap<RegisterModel, User>();
+            CreateMap<UpdateModel, User>();
+        }
+    }
+}
---
+++ Models/Users/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Siace.Models.Users
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
